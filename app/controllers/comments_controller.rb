b36ce7866fbb2ce4c53baf3ff08d39a7c22fb88e
saurav7373 <dreamsaurav7373@gmail.com>
2020-10-05 22:49:29 +0545

    work on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,47 @@
 class CommentsController < ApplicationController
+  before_action :find_message, only: [:create, :edit, :update, :destroy]
+  before_action :find_message, only: [:edit ,:update ,:destroy]
 
   def create
-    @message = Message.find(params[:message_id])
-    #finding message and create comment according to the message id
-    @comment = @message.comment.create(comment_params)
-  #  findig comment a/c to current user
-    @comment.user_id = current.user_id
+    # creates a comment with respect to the message
+    @comment = @message.comments.create(comment_params)
+    @comment.user_id = current_user.id
 
-    if comment.save
+    if @comment.save
       redirect_to message_path(@message)
     else
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
+
   private
+
   def comment_params
     params.require(:comment).permit(:content)
   end
+
+  def find_message
+    @message = Message.find(params[:message_id])
+  end
+
+  def find_comment
+    @comment = @message.comments.find(params[:id])
+  end
+
 end
