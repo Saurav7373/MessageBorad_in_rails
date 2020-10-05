3f083afce32a5c1322be6e994df215ad36e21cc7
saurav7373 <dreamsaurav7373@gmail.com>
2020-10-05 13:41:23 +0545

    Message CRUD

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all.order("created_at DESC")
   end
 
-
   def new
     @message = Message.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   def create
@@ -15,13 +33,15 @@ class MessagesController < ApplicationController
     else
       redirect 'new'
     end
-
   end
 
   private
 
-
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
